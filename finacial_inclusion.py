2d39989f16c35ec1fe9ec1044739c8e4f9469f0e
Ziyah11 <113618608+Ziyah11@users.noreply.github.com>
2024-02-23 17:51:24 +0100

    Update finacial_inclusion.py

--- finacial_inclusion.py
+++ finacial_inclusion.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 import streamlit as st
 from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 import joblib
 encoder = LabelEncoder()
 scaler = StandardScaler()
